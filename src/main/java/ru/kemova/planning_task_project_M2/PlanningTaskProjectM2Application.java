3ff9302a0ee7f9f39037fdbe293e34d476eebf6a
IrenXX <irinareg98@gmail.com>
2024-09-18 12:22:15 +0300

    Конфигурация проекта Mail Sender

---
+++ src/main/java/ru/kemova/planning_task_project_M2/PlanningTaskProjectM2Application.java
@@ -0,0 +1,13 @@
+package ru.kemova.planning_task_project_M2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlanningTaskProjectM2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlanningTaskProjectM2Application.class, args);
+	}
+
+}
---
+++ src/test/java/ru/kemova/planning_task_project_M2/PlanningTaskProjectM2ApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.kemova.planning_task_project_M2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlanningTaskProjectM2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
